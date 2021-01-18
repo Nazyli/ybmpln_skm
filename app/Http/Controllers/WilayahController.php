42607c17282ff51090b7780597fe93658eb2654b
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2021-01-18 17:13:48 +0700

    add total and dinamis table area

--- app/Http/Controllers/WilayahController.php
+++ app/Http/Controllers/WilayahController.php
@@ -7,6 +7,7 @@ use App\Kabupaten;
 use App\Kecamatan;
 use App\Provinsi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WilayahController extends Controller
 {
@@ -90,23 +91,48 @@ class WilayahController extends Controller
     }
     public function provinsi()
     {
-        $prov = Provinsi::all();
+        $prov = DB::table('provinsi')
+        ->join('kabupaten', 'provinsi.id', '=', 'kabupaten.id_provinsi')
+        ->join('kecamatan', 'kabupaten.id', '=', 'kecamatan.id_kabupaten')
+        ->join('desa', 'kecamatan.id', '=', 'desa.id_kecamatan')
+        ->leftJoin('pendaftar', 'desa.id', '=', 'pendaftar.id_desa_sekarang')
+        ->select('provinsi.*', DB::raw('count(pendaftar.id_desa_sekarang) as total'))
+        ->groupBy('provinsi.id')
+        ->get();
         return json_encode($prov);
     }
 
     public function kabupaten($id)
     {
-        $kab = Kabupaten::where('id_provinsi', $id)->get();
+        $kab = DB::table('kabupaten')
+        ->join('kecamatan', 'kabupaten.id', '=', 'kecamatan.id_kabupaten')
+        ->join('desa', 'kecamatan.id', '=', 'desa.id_kecamatan')
+        ->leftJoin('pendaftar', 'desa.id', '=', 'pendaftar.id_desa_sekarang')
+        ->select('kabupaten.*', DB::raw('count(pendaftar.id_desa_sekarang) as total'))
+        ->where('kabupaten.id_provinsi', '=', $id)
+        ->groupBy('kabupaten.id')
+        ->get();
         return json_encode($kab);
     }
     public function kecamatan($id)
     {
-        $kec = Kecamatan::where('id_kabupaten', $id)->get();
+        $kec = DB::table('kecamatan')
+        ->join('desa', 'kecamatan.id', '=', 'desa.id_kecamatan')
+        ->leftJoin('pendaftar', 'desa.id', '=', 'pendaftar.id_desa_sekarang')
+        ->select('kecamatan.*', DB::raw('count(pendaftar.id_desa_sekarang) as total'))
+        ->where('kecamatan.id_kabupaten', '=', $id)
+        ->groupBy('kecamatan.id')
+        ->get();
         return json_encode($kec);
     }
     public function desa($id)
     {
-        $desa = Desa::where('id_kecamatan', $id)->get();
+        $desa = DB::table('desa')
+        ->leftJoin('pendaftar', 'desa.id', '=', 'pendaftar.id_desa_sekarang')
+        ->select('desa.*', DB::raw('count(pendaftar.id_desa_sekarang) as total'))
+        ->where('desa.id_kecamatan', '=', $id)
+        ->groupBy('desa.id')
+        ->get();
         return json_encode($desa);
     }
 }
